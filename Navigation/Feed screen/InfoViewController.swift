b16238c697cdf36f67a7c1bb8f80db579642ff97
Natali Malich <nv.malich@gmail.com>
2022-02-13 21:44:15 +0000

    task 1.2.3 completed

--- Navigation/Feed screen/InfoViewController.swift
+++ Navigation/Feed screen/InfoViewController.swift
@@ -23,9 +23,20 @@ class InfoViewController: UIViewController {
     
     private lazy var label: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 2
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private var nameResidents = [String?]()
+    
+    private lazy var residentID = String(describing: TableViewCellResident.self)
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.backgroundColor = .white
+        tableView.layer.cornerRadius = 10
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
     }()
     
     override func viewDidLoad() {
@@ -35,13 +46,19 @@ class InfoViewController: UIViewController {
         setupViews()
         setupConstraints()
         
-        NetworkService.performRequest(returnedTitle: {(result: String) in
-            self.label.text = result})
+        tableView.register(TableViewCellResident.self, forCellReuseIdentifier: residentID)
+        tableView.dataSource = self
+        
+        NetworkService.getResidents(returnedResinents: {(result: [String]) in
+            self.label.text = "Rundom name: \(result.randomElement() ?? "")"
+            self.nameResidents = result
+            self.tableView.reloadData()
+        })
     }
 }
 extension InfoViewController {
     private func setupViews() {
-        [button, label]
+        [button, label, tableView]
             .forEach {view.addSubview($0)}
     }
 }
@@ -49,14 +66,35 @@ extension InfoViewController {
 extension InfoViewController {
     private func setupConstraints() {
         [
-            label.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -50),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.heightAnchor.constraint(equalToConstant: 50),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50),
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             button.heightAnchor.constraint(equalToConstant: 50),
-            button.widthAnchor.constraint(equalToConstant: 300)
+            button.widthAnchor.constraint(equalToConstant: 300),
+            tableView.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 50),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
         ]
             .forEach {$0.isActive = true}
+    }
+}
+
+extension InfoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameResidents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TableViewCellResident = tableView.dequeueReusableCell(withIdentifier: residentID, for: indexPath) as! TableViewCellResident
+        if let nameResidents = nameResidents[indexPath.row] {
+            cell.nameResident.text = nameResidents.description
+            cell.selectionStyle = .none
+        } else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }
--- Navigation/NetworkService/NetworkService.swift
+++ Navigation/NetworkService/NetworkService.swift
@@ -11,19 +11,47 @@ struct NetworkService {
     
     private static var url = URL(string: "https://swapi.dev/api/planets/1")
     
-    static func performRequest(returnedTitle: @escaping (String) -> Void) {
+    static func getResidents(returnedResinents: @escaping ([String]) -> Void) {
         guard let url = url else { return }
         let session = URLSession(configuration: .default)
         session.dataTask(with: url) { data, responce, error in
             if let data = data {
                 do {
                     let planet = try JSONDecoder().decode(Planet.self, from: data)
-                    DispatchQueue.main.async {
-                        returnedTitle(planet.orbitalPeriod)
+                    let residentsUrls = planet.residents
+                    var residentsNames = [String]()
+                    var count = 0
+                    for url in residentsUrls {
+                        getNameResident(url: url) { name in
+                            residentsNames.append(name)
+                            count += 1
+                            if count == residentsUrls.count {
+                                DispatchQueue.main.async {
+                                    returnedResinents(residentsNames)
+                                }
+                            }
+                        }
                     }
                 }
                 catch let error {
                     print("Web service didn't respond: \(error.localizedDescription)")
+                }
+                return
+            }
+        }.resume()
+    }
+    
+    private static func getNameResident(url: String, returnedName: @escaping (String) -> Void) {
+        guard let url = URL(string: url) else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, responce, error in
+            if let data = data {
+                do {
+                    let resident = try JSONDecoder().decode(Resident.self, from: data)
+                    returnedName(resident.name)
+                }
+                catch let error {
+                    print(error.localizedDescription)
                 }
                 return
             }
