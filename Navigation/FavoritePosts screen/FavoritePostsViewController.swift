f0109dbfe6b3c068fc2e38eca87e7b574e968c72
Natali Malich <nv.malich@gmail.com>
2022-05-03 22:15:35 +0100

    added search

--- Navigation/FavoritePosts screen/FavoritePostsViewController.swift
+++ Navigation/FavoritePosts screen/FavoritePostsViewController.swift
@@ -18,6 +18,14 @@ class FavoritePostsViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.barStyle = .default
+        searchBar.placeholder = "Search by author"
+        searchBar.sizeToFit()
+        return searchBar
+    }()
    
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -36,7 +44,10 @@ class FavoritePostsViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
-        setUpGestureRecognizer()
+        searchBar.delegate = self
+        
+//Удаление поста из избранного по двойному нажатию на пост
+//        setUpGestureRecognizer()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -65,12 +76,12 @@ extension FavoritePostsViewController {
 
 extension FavoritePostsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return DataBaseManager.shared.getFavoritePosts().count
+        return DataBaseManager.shared.getCountPosts()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: PostTableViewCell = tableView.dequeueReusableCell(withIdentifier: postID, for: indexPath) as! PostTableViewCell
-        cell.post = DataBaseManager.shared.getFavoritePosts()[indexPath.row]
+        cell.post = DataBaseManager.shared.getFavoritePostsWithPredicate(author: searchBar.text)[indexPath.row]
         cell.iconLikes.image = UIImage(systemName: "heart.fill")
         cell.iconLikes.tintColor = .red
         return cell
@@ -79,23 +90,47 @@ extension FavoritePostsViewController: UITableViewDataSource, UITableViewDelegat
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        DataBaseManager.shared.removeFavoritePost(post: DataBaseManager.shared.getFavoritePostsWithPredicate(author: nil)[indexPath.row])
+        tableView.reloadData()
+    }
 }
 
-extension FavoritePostsViewController {
-    private func setUpGestureRecognizer() {
-        let doubleTapGestureOnCell = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
-        doubleTapGestureOnCell.numberOfTapsRequired = 2
-        tableView.addGestureRecognizer(doubleTapGestureOnCell)
+extension FavoritePostsViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = nil
+        searchBar.resignFirstResponder()
+        DataBaseManager.shared.getFavoritePostsWithPredicate(author: searchBar.text)
+        tableView.reloadData()
     }
     
-    @objc private func handleDoubleTap(_ tapGesture: UITapGestureRecognizer) {
-        if tapGesture.state == .ended {
-            let location = tapGesture.location(in: self.tableView)
-            if let indexPath = tableView.indexPathForRow(at: location), let cell = tableView.cellForRow(at: indexPath) as? PostTableViewCell, let post = cell.post {
-                DataBaseManager.shared.removeFavoritePost(post: post)
-                tableView.reloadData()
-                print("Post removed")
-            }
-        }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        DataBaseManager.shared.getFavoritePostsWithPredicate(author: searchText)
+        tableView.reloadData()
     }
 }
+
+//extension FavoritePostsViewController {
+//    private func setUpGestureRecognizer() {
+//        let doubleTapGestureOnCell = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+//        doubleTapGestureOnCell.numberOfTapsRequired = 2
+//        tableView.addGestureRecognizer(doubleTapGestureOnCell)
+//    }
+//
+//    @objc private func handleDoubleTap(_ tapGesture: UITapGestureRecognizer) {
+//        if tapGesture.state == .ended {
+//            let location = tapGesture.location(in: self.tableView)
+//            if let indexPath = tableView.indexPathForRow(at: location), let cell = tableView.cellForRow(at: indexPath) as? PostTableViewCell, let post = cell.post {
+//                DataBaseManager.shared.removeFavoritePost(post: post)
+//                tableView.reloadData()
+//                print("Post removed")
+//            }
+//        }
+//    }
+//}
